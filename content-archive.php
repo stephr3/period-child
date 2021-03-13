b2217f2202481f3791403ac5b4e0eeb71d97e2e4
Stephanie Roth <stephr3@gmail.com>
2021-03-13 14:54:47 +0900

    add initial content displayed on classes archive page

--- content-archive.php
+++ content-archive.php
@@ -10,7 +10,36 @@
 				</h2>
 			</div>
 			<div class="post-content">
-				<?php the_field('details'); ?>
+				<ul class="no-bullets">
+					<?php if( get_field('volunteers_needed') == 'Yes' ): ?>
+						<li class="vols-needed-li"><i>* Volunteers Needed *</i></li>
+					<?php endif ?>
+
+					<?php if( get_field('class_format') ): ?>
+						<li><b>Format:</b> <?php the_field('class_format'); ?></li>
+					<?php endif ?>
+
+					<?php if( get_field('class_type') ): ?>
+						<li><b>Type:</b> <?php the_field('class_type'); ?></li>
+					<?php endif ?>
+
+					<?php if( get_field('level') ): ?>
+						<li><b>Level:</b> <?php the_field('level'); ?></li>
+					<?php endif ?>
+
+					<?php if( get_field('cost') ): ?>
+						<li><b>Cost:</b> <?php the_field('cost'); ?></li>
+					<?php endif ?>
+
+					<?php if( get_field('days_offered') ): ?>
+						<li><b>Days:</b> <?php the_field('days_offered'); ?></li>
+					<?php endif ?>
+
+					<?php if( get_field('time_of_day') ): ?>
+						<li><b>Times:</b> <?php the_field('time_of_day'); ?></li>
+					<?php endif ?>
+				</ul>
+				<a href="<?php echo esc_url( get_permalink() ); ?>">More details...</a>				
 			</div>
 		</div>
 	</article>
