f809c95a8acf87a1305a0edc2f8a6ae2d6d260ff
Deana <demaisja@gmail.com>
2021-03-29 21:48:36 -0700

    Section headers generated from field group content. Resource tags are the content

--- archive-studentresource.php
+++ archive-studentresource.php
@@ -5,10 +5,24 @@ get_template_part( 'content/archive-header' );
 do_action( 'after_archive_header' ); ?>
 
 <div id="loop-container" class="loop-container">
-	<div class="filters-container"><h2>Placeholder for Future Titles based on Custom Query</h2></div>
+	
+    <?php
+        $key = 'tag';
+        $field = get_field_object($key); 
+    if ($field) {
+       foreach ($field['choices'] as $key => $value) {
+        ?>
+        <div class="filters-container"><h2> <?php echo ($value);  ?> </h2></div>        
+       <?php 
+             
+      }                             
+   }
+?>
+      
     <?php
     while(have_posts()) {
         the_post(); 
+        
         get_template_part('content-archive-resources');
        }
     ?>
