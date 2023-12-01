c2ba40c91c04125a3b0f4cdf4c6b973bd55f4d67
Stephanie Roth <stephr3@gmail.com>
2023-12-01 08:47:05 -0800

    updated files dec 2023

--- functions.php
+++ functions.php
@@ -1,67 +1,67 @@
-<?php 
+<?php
 
     add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
     function enqueue_parent_styles() {
         wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    } 
+    }
 
-    add_action( 'wp_enqueue_scripts', 'enable_frontend_dashicons' );  
+    add_action( 'wp_enqueue_scripts', 'enable_frontend_dashicons' );
     function enable_frontend_dashicons() {
       wp_enqueue_style( 'dashicons' );
     }
-  
+
     add_action( 'wp_footer', 'enqueue_parent_scripts' );
     function enqueue_parent_scripts() {
         wp_enqueue_script('parent_scripts', get_stylesheet_directory_uri() . '/js/search-page.js', NULL, '1.0', true);
-        wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=' . getenv("Google_Map_API_Key") , NULL, '1.0', true);
+        wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=KEY' , NULL, '1.0', true);
         wp_enqueue_script('GoogleMapHelper', get_stylesheet_directory_uri() . '/js/google-map-helper.js', NULL, '1.0', true);
-        wp_enqueue_style( 'dashicons' );
     }
 
+	//ACF google maps api key set
     add_filter('acf/fields/google_map/api', 'googleMapKey');
     function googleMapKey($api) {
-        $api['key'] = getenv("Google_Map_API_Key");
+        $api['key'] = "KEY";
         return $api;
     }
 
-    // Creating the widget 
+    // Creating the widget
     class wpb_widget extends WP_Widget {
-      
+
     function __construct() {
         parent::__construct(
-          
+
         // Base ID of your widget
-        'wpb_widget', 
-          
+        'wpb_widget',
+
         // Widget name will appear in UI
-        __('Classes Filters Widget', 'wpb_widget_domain'), 
-          
+        __('Classes Filters Widget', 'wpb_widget_domain'),
+
         // Widget description
-        array( 'description' => __( 'Add filter UI and functionality to `Classes` custom data type' ), ) 
+        array( 'description' => __( 'Add filter UI and functionality to `Classes` custom data type' ), )
         );
     }
-      
+
     // Creating widget front-end
-      
+
     public function widget( $args, $instance ) {
         $title = apply_filters( 'widget_title', $instance['title'] );
-          
+
         // before and after widget arguments are defined by themes
         echo $args['before_widget'];
         // echo $args['before_title'] . "Filters" . $args['after_title'];
-          
+
         // This is where you run the code and display the output
         // echo __( 'Hello, World!', 'wpb_widget_domain' );
         ?>
             <div><?php include "filtersform.php"; ?></div>
-        <?php 
+        <?php
         echo $args['after_widget'];
     }
-     
+
     // Class wpb_widget ends here
-    } 
-     
-     
+    }
+
+
     // Register and load the widget
     function wpb_load_widget() {
         register_widget( 'wpb_widget' );
